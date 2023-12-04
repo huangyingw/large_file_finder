8106128c29481a4b39a534b3634c2d6c01baafd7
Ying Huang <huangyingw@gmail.com>
2023-12-03 18:14:18 -0800

    n

--- utils.go
+++ utils.go
@@ -5,9 +5,14 @@ package main
 
 import (
 	"bufio"
+	"context"
 	"fmt"
+	"github.com/go-redis/redis/v8"
 	"os"
+	"path/filepath"
+	"regexp"
 	"sort"
+	"strings"
 )
 
 func loadExcludePatterns(filename string) ([]string, error) {
@@ -36,5 +41,30 @@ func sortKeys(keys []string, data map[string]FileInfo, sortByModTime bool) {
 		sort.Slice(keys, func(i, j int) bool {
 			return data[keys[i]].Size > data[keys[j]].Size
 		})
+	}
+}
+
+func compileExcludePatterns(filename string) ([]*regexp.Regexp, error) {
+	excludePatterns, err := loadExcludePatterns(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	excludeRegexps := make([]*regexp.Regexp, len(excludePatterns))
+	for i, pattern := range excludePatterns {
+		regexPattern := strings.Replace(pattern, "*", ".*", -1)
+		excludeRegexps[i], err = regexp.Compile(regexPattern)
+		if err != nil {
+			return nil, fmt.Errorf("Invalid regex pattern '%s': %v", regexPattern, err)
+		}
+	}
+	return excludeRegexps, nil
+}
+
+func performSaveOperation(rootDir, filename string, sortByModTime bool, rdb *redis.Client, ctx context.Context) {
+	if err := saveToFile(rootDir, filename, sortByModTime, rdb, ctx); err != nil {
+		fmt.Printf("Error saving to %s: %s\n", filepath.Join(rootDir, filename), err)
+	} else {
+		fmt.Printf("Saved data to %s\n", filepath.Join(rootDir, filename))
 	}
 }
