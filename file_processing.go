149d7c4c893f0369090d9b216dc4d72100ac0e12
Ying Huang <huangyingw@gmail.com>
2023-12-03 23:23:31 -0800

    n

--- file_processing.go
+++ file_processing.go
@@ -4,21 +4,30 @@ package main
 import (
 	"bytes"
 	"context"
+	"crypto/sha256"
 	"encoding/gob"
 	"fmt"
 	"github.com/go-redis/redis/v8"
+	"io"
 	"os"
 	"path/filepath"
 	"sync/atomic"
 )
 
-func saveToFile(dir, filename string, sortByModTime bool, rdb *redis.Client, ctx context.Context) error {
-	file, err := os.Create(filepath.Join(dir, filename))
+func getFileInfoFromRedis(rdb *redis.Client, ctx context.Context, hashedKey string) (FileInfo, error) {
+	var fileInfo FileInfo
+	value, err := rdb.Get(ctx, hashedKey).Bytes()
 	if err != nil {
-		return err
+		return fileInfo, err
 	}
-	defer file.Close()
 
+	buf := bytes.NewBuffer(value)
+	dec := gob.NewDecoder(buf)
+	err = dec.Decode(&fileInfo)
+	return fileInfo, err
+}
+
+func saveToFile(dir, filename string, sortByModTime bool, rdb *redis.Client, ctx context.Context) error {
 	iter := rdb.Scan(ctx, 0, "*", 0).Iterator()
 	var data = make(map[string]FileInfo)
 	for iter.Next(ctx) {
@@ -27,35 +36,26 @@ func saveToFile(dir, filename string, sortByModTime bool, rdb *redis.Client, ctx
 		if err != nil {
 			continue
 		}
-		value, err := rdb.Get(ctx, hashedKey).Bytes()
-		if err != nil {
-			continue
-		}
-		var fileInfo FileInfo
-		buf := bytes.NewBuffer(value)
-		dec := gob.NewDecoder(buf)
-		if err := dec.Decode(&fileInfo); err == nil {
+
+		fileInfo, err := getFileInfoFromRedis(rdb, ctx, hashedKey)
+		if err == nil {
 			data[originalPath] = fileInfo
 		}
 	}
 
+	var lines []string
 	var keys []string
 	for k := range data {
 		keys = append(keys, k)
 	}
-
 	sortKeys(keys, data, sortByModTime)
-
 	for _, k := range keys {
 		relativePath, _ := filepath.Rel(dir, k)
-		if sortByModTime {
-			utcTimestamp := data[k].ModTime.UTC().Unix()
-			fmt.Fprintf(file, "%d,\"./%s\"\n", utcTimestamp, relativePath)
-		} else {
-			fmt.Fprintf(file, "%d,\"./%s\"\n", data[k].Size, relativePath)
-		}
+		line := formatFileInfoLine(data[k], relativePath, sortByModTime)
+		lines = append(lines, line)
 	}
-	return nil
+
+	return writeLinesToFile(filepath.Join(dir, filename), lines)
 }
 
 func processFile(path string, typ os.FileMode, rdb *redis.Client, ctx context.Context, startTime int64) {
@@ -93,6 +93,13 @@ func processFile(path string, typ os.FileMode, rdb *redis.Client, ctx context.Co
 		return
 	}
 
+	// 计算文件的SHA-256哈希值
+	fileHash, err := calculateFileHash(path)
+	if err != nil {
+		fmt.Printf("Error calculating hash for file %s: %s\n", path, err)
+		return
+	}
+
 	// 使用管道批量处理Redis命令
 	pipe := rdb.Pipeline()
 
@@ -100,12 +107,36 @@ func processFile(path string, typ os.FileMode, rdb *redis.Client, ctx context.Co
 	pipe.Set(ctx, hashedKey, buf.Bytes(), 0)
 	pipe.Set(ctx, "path:"+hashedKey, path, 0)
 	pipe.Set(ctx, "updateTime:"+hashedKey, startTime, 0)
+	pipe.Set(ctx, "hash:"+hashedKey, fileHash, 0) // 存储文件哈希值
 
 	if _, err = pipe.Exec(ctx); err != nil {
 		fmt.Printf("Error executing pipeline for file: %s: %s\n", path, err)
 		return
 	}
 
+}
+
+func formatFileInfoLine(fileInfo FileInfo, relativePath string, sortByModTime bool) string {
+	if sortByModTime {
+		utcTimestamp := fileInfo.ModTime.UTC().Unix()
+		return fmt.Sprintf("%d,\"./%s\"", utcTimestamp, relativePath)
+	}
+	return fmt.Sprintf("%d,\"./%s\"", fileInfo.Size, relativePath)
+}
+
+// calculateFileHash 计算文件的SHA-256哈希值
+func calculateFileHash(filePath string) (string, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
+	hasher := sha256.New()
+	if _, err := io.Copy(hasher, file); err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%x", hasher.Sum(nil)), nil
 }
 
 func processDirectory(path string) {
