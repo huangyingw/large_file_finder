0e1a662b23250a1697bd60e010d021d0bcba742d
Ying Huang <huangyingw@gmail.com>
2023-12-06 00:04:41 -0800

    n

--- utils.go
+++ utils.go
@@ -6,6 +6,7 @@ package main
 import (
 	"bufio"
 	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/go-redis/redis/v8"
 	"os"
@@ -93,13 +94,30 @@ func findAndLogDuplicates(rootDir string, outputFile string, rdb *redis.Client, 
 		return err
 	}
 
-	var lines []string
+	// 创建一个切片来存储哈希和对应的文件大小
+	type hashSize struct {
+		hash string
+		size int64
+	}
+	var hashSizes []hashSize
 	for hash, paths := range hashes {
 		if len(paths) > 1 {
-			fmt.Printf("Found duplicate: Hash %s with %d occurrences\n", hash, len(paths))
-			lines = append(lines, fmt.Sprintf("Duplicate files for hash %s:", hash))
-			lines = append(lines, paths...) // 将所有重复文件的路径添加到列表中
+			// 假设每个哈希下的第一个路径代表整个组
+			size, _ := getFileSizeFromRedis(rdb, ctx, generateHash(paths[0])) // 直接使用第一个路径
+			hashSizes = append(hashSizes, hashSize{hash: hash, size: size})
 		}
+	}
+
+	// 根据文件大小排序哈希
+	sort.Slice(hashSizes, func(i, j int) bool {
+		return hashSizes[i].size < hashSizes[j].size
+	})
+
+	var lines []string
+	for _, hs := range hashSizes {
+		paths := hashes[hs.hash]
+		lines = append(lines, fmt.Sprintf("Duplicate files for hash %s:", hs.hash))
+		lines = append(lines, paths...)
 	}
 
 	if len(lines) == 0 {
@@ -117,4 +135,19 @@ func findAndLogDuplicates(rootDir string, outputFile string, rdb *redis.Client, 
 
 	fmt.Printf("Duplicates written to '%s'\n", outputFile)
 	return nil
+}
+
+func getFileSizeFromRedis(rdb *redis.Client, ctx context.Context, hashedKey string) (int64, error) {
+	fileInfoData, err := rdb.Get(ctx, hashedKey).Bytes()
+	if err != nil {
+		return 0, err
+	}
+
+	var fileInfo FileInfo
+	err = json.Unmarshal(fileInfoData, &fileInfo)
+	if err != nil {
+		return 0, err
+	}
+
+	return fileInfo.Size, nil
 }
