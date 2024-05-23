8e8da0b56079e7742705774a3775b421a1c4a783
Ying Huang <huangyingw@gmail.com>
2024-05-22 19:17:13 -0700

    n

--- find_redis_string.sh
+++ find_redis_string.sh
@@ -6,7 +6,9 @@ search_string="/media/av162/av/旬果/test/test.mp4"
 # 使用 Redis SCAN 命令查找所有包含特定字符串的键和值
 redis-cli --scan --pattern "*" | while read -r key; do
 value=$(redis-cli GET "$key" | tr -d '\000')
-if [[ "$value" == *"$search_string"* ]]; then
+
+# 检查键或值是否包含指定的字符串
+if [[ "$key" == *"$search_string"* ]] || [[ "$value" == *"$search_string"* ]]; then
     echo "Key: $key, Value: $value"
 fi
 done
