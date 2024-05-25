c1daaa12bd6771e358e5c534ec1bccb9e8bbb34f
Ying Huang <huangyingw@gmail.com>
2024-05-24 18:11:14 -0700

    n

--- redis_client.go
+++ redis_client.go
@@ -51,7 +51,7 @@ func saveFileInfoToRedis(rdb *redis.Client, ctx context.Context, hashedKey strin
 		pipe.Set(ctx, "hashedKeyToFullHash:"+hashedKey, fullHash, 0) // 存储完整文件哈希值
 	}
 	// 存储从路径到hashedKey的映射
-	pipe.Set(ctx, "pathToHash:"+path, hashedKey, 0)
+	pipe.Set(ctx, "pathToHashedKey:"+path, hashedKey, 0)
 
 	if _, err := pipe.Exec(ctx); err != nil {
 		return fmt.Errorf("error executing pipeline for file: %s: %w", path, err)
@@ -61,12 +61,12 @@ func saveFileInfoToRedis(rdb *redis.Client, ctx context.Context, hashedKey strin
 
 func cleanUpOldRecords(rdb *redis.Client, ctx context.Context) error {
 	fmt.Println("Starting to clean up old records")
-	iter := rdb.Scan(ctx, 0, "pathToHash:*", 0).Iterator()
+	iter := rdb.Scan(ctx, 0, "pathToHashedKey:*", 0).Iterator()
 	for iter.Next(ctx) {
 		pathToHashKey := iter.Val()
 
 		// 解析出文件路径
-		filePath := strings.TrimPrefix(pathToHashKey, "pathToHash:")
+		filePath := strings.TrimPrefix(pathToHashKey, "pathToHashedKey:")
 
 		// 检查文件是否存在
 		if _, err := os.Stat(filePath); os.IsNotExist(err) {
@@ -87,7 +87,7 @@ func cleanUpOldRecords(rdb *redis.Client, ctx context.Context) error {
 
 func cleanUpRecordsByFilePath(rdb *redis.Client, ctx context.Context, filePath string) error {
 	// 获取 hashedKey
-	hashedKey, err := rdb.Get(ctx, "pathToHash:"+filePath).Result()
+	hashedKey, err := rdb.Get(ctx, "pathToHashedKey:"+filePath).Result()
 	if err != nil && err != redis.Nil {
 		return fmt.Errorf("error retrieving hashedKey for path %s: %v", filePath, err)
 	}
@@ -112,7 +112,7 @@ func cleanUpRecordsByFilePath(rdb *redis.Client, ctx context.Context, filePath s
 	pipe.Del(ctx, "updateTime:"+hashedKey)      // 删除 updateTime 键
 	pipe.Del(ctx, "fileInfo:"+hashedKey)        // 删除 fileInfo 相关数据
 	pipe.Del(ctx, "hashedKeyToPath:"+hashedKey)            // 删除 path 相关数据
-	pipe.Del(ctx, "pathToHash:"+filePath)       // 删除从路径到 hashedKey 的映射
+	pipe.Del(ctx, "pathToHashedKey:"+filePath)       // 删除从路径到 hashedKey 的映射
 	pipe.Del(ctx, "hashedKeyToFullHash:"+hashedKey)        // 删除完整文件哈希相关数据
 	pipe.ZRem(ctx, duplicateFilesKey, filePath) // 从 duplicateFiles 有序集合中移除路径
 	pipe.SRem(ctx, "hash:"+fileHash, filePath)  // 从 hash 集合中移除文件路径
