18b17fd8e946a5c246e130125a110eece26c35de
Ying Huang <huangyingw@gmail.com>
2024-05-25 14:28:42 -0700

    n

--- file_processing.go
+++ file_processing.go
@@ -9,6 +9,7 @@ import (
 	"fmt"
 	"github.com/go-redis/redis/v8"
 	"io"
+	"log"
 	"os"
 	"path/filepath"
 	"sort"
@@ -46,7 +47,7 @@ func saveToFile(dir, filename string, sortByModTime bool, rdb *redis.Client, ctx
 
 		fileInfo, err := getFileInfoFromRedis(rdb, ctx, hashedKey)
 		if err != nil {
-			fmt.Printf("Error getting file info from Redis for key %s: %s\n", hashedKey, err)
+			log.Printf("Error getting file info from Redis for key %s: %s\n", hashedKey, err)
 			continue
 		}
 
@@ -55,7 +56,7 @@ func saveToFile(dir, filename string, sortByModTime bool, rdb *redis.Client, ctx
 	}
 
 	if !foundData {
-		fmt.Println("No data found in Redis.")
+		log.Println("No data found in Redis.")
 		return nil
 	}
 
@@ -72,17 +73,16 @@ func saveToFile(dir, filename string, sortByModTime bool, rdb *redis.Client, ctx
 	}
 
 	if len(lines) == 0 {
-		fmt.Println("No lines to write to file.")
+		log.Println("No lines to write to file.")
 		return nil
 	}
 
-	fmt.Printf("Writing %d lines to file %s\n", len(lines), filepath.Join(dir, filename))
+	log.Printf("Writing %d lines to file %s\n", len(lines), filepath.Join(dir, filename))
 	return writeLinesToFile(filepath.Join(dir, filename), lines)
 }
 
 func processFile(path string, typ os.FileMode, rdb *redis.Client, ctx context.Context, startTime int64) {
-	// Update progress counter atomically
-	atomic.AddInt32(&progressCounter, 1)
+	defer atomic.AddInt32(&progressCounter, 1)
 
 	// 生成文件路径的哈希作为键
 	hashedKey := generateHash(path)
@@ -90,40 +90,39 @@ func processFile(path string, typ os.FileMode, rdb *redis.Client, ctx context.Co
 	// 检查Redis中是否已存在该文件的信息
 	exists, err := rdb.Exists(ctx, "fileInfo:"+hashedKey).Result()
 	if err != nil {
-		fmt.Printf("Error checking existence in Redis for file %s: %s\n", path, err)
+		log.Printf("Error checking existence in Redis for file %s: %s\n", path, err)
 		return
 	}
 	if exists > 0 {
-		// 文件已存在于Redis中，跳过处理
-		fmt.Printf("File %s already exists in Redis, skipping processing.\n", path)
+		log.Printf("File %s already exists in Redis, skipping processing.\n", path)
 		return
 	}
 
-	fmt.Printf("File %s not found in Redis, processing.\n", path) // 添加打印信息
+	log.Printf("File %s not found in Redis, processing.\n", path)
 
 	info, err := os.Stat(path)
 	if err != nil {
-		fmt.Printf("Error stating file: %s, Error: %s\n", path, err)
+		log.Printf("Error stating file: %s, Error: %s\n", path, err)
 		return
 	}
 
 	var buf bytes.Buffer
 	enc := gob.NewEncoder(&buf)
 	if err := enc.Encode(FileInfo{Size: info.Size(), ModTime: info.ModTime()}); err != nil {
-		fmt.Printf("Error encoding: %s, File: %s\n", err, path)
+		log.Printf("Error encoding: %s, File: %s\n", err, path)
 		return
 	}
 
 	// 计算文件的SHA-512哈希值（只读取前4KB）
 	fileHash, err := calculateFileHash(path, false)
 	if err != nil {
-		fmt.Printf("Error calculating hash for file %s: %s\n", path, err)
+		log.Printf("Error calculating hash for file %s: %s\n", path, err)
 		return
 	}
 
 	// 调用saveFileInfoToRedis函数来保存文件信息到Redis，不需要完整文件的哈希值
 	if err := saveFileInfoToRedis(rdb, ctx, hashedKey, path, buf, fileHash, ""); err != nil {
-		fmt.Printf("Error saving file info to Redis for file %s: %s\n", path, err)
+		log.Printf("Error saving file info to Redis for file %s: %s\n", path, err)
 		return
 	}
 }
@@ -135,8 +134,8 @@ func formatFileInfoLine(fileInfo FileInfo, relativePath string, sortByModTime bo
 	return fmt.Sprintf("%d,\"./%s\"", fileInfo.Size, relativePath)
 }
 
-// calculateFileHash 计算文件的 SHA-512 哈希值
-// 读取前4KB的数据，除非fullRead参数为true
+const readLimit = 100 * 1024 // 100KB
+
 func calculateFileHash(path string, fullRead bool) (string, error) {
 	file, err := os.Open(path)
 	if err != nil {
@@ -146,16 +145,11 @@ func calculateFileHash(path string, fullRead bool) (string, error) {
 
 	hasher := sha512.New()
 	if fullRead {
-		// 打印正在计算完整哈希的文件路径
-		fmt.Printf("Calculating full hash for file: %s\n", path)
-
-		// 读取整个文件
+		log.Printf("Calculating full hash for file: %s\n", path)
 		if _, err := io.Copy(hasher, file); err != nil {
 			return "", err
 		}
 	} else {
-		// 只读取前100KB的数据
-		const readLimit = 100 * 1024
 		reader := io.LimitReader(file, readLimit)
 		if _, err := io.Copy(hasher, reader); err != nil {
 			return "", err
@@ -166,15 +160,11 @@ func calculateFileHash(path string, fullRead bool) (string, error) {
 }
 
 func processDirectory(path string) {
-	// 处理目录的逻辑
-	fmt.Printf("Processing directory: %s\n", path)
-	// 可能的操作：遍历目录下的文件等
+	log.Printf("Processing directory: %s\n", path)
 }
 
 func processSymlink(path string) {
-	// 处理软链接的逻辑
-	fmt.Printf("Processing symlink: %s\n", path)
-	// 可能的操作：解析软链接，获取实际文件等
+	log.Printf("Processing symlink: %s\n", path)
 }
 
 func processKeyword(keyword string, keywordFiles []string, rdb *redis.Client, ctx context.Context, rootDir string) {
@@ -197,41 +187,30 @@ func processKeyword(keyword string, keywordFiles []string, rdb *redis.Client, ct
 	outputFilePath := filepath.Join(rootDir, keyword+".txt")
 	outputFile, err := os.Create(outputFilePath)
 	if err != nil {
-		fmt.Println("Error creating file:", err)
+		log.Println("Error creating file:", err)
 		return
 	}
 	defer outputFile.Close() // 确保文件会被关闭
 
 	_, err = outputFile.WriteString(outputData.String())
 	if err != nil {
-		fmt.Println("Error writing to file:", err)
+		log.Println("Error writing to file:", err)
 	}
 }
 
 // cleanPath 用于清理和标准化路径
 func cleanPath(path string) string {
-	// 先去除路径开头的引号（如果存在）
-	if strings.HasPrefix(path, `"`) {
-		path = strings.TrimPrefix(path, `"`)
-	}
-
-	// 再去除 "./"（如果路径以 "./" 开头）
+	path = strings.Trim(path, `"`)
 	if strings.HasPrefix(path, "./") {
 		path = strings.TrimPrefix(path, "./")
 	}
-
-	// 最后去除路径末尾的引号（如果存在）
-	if strings.HasSuffix(path, `"`) {
-		path = strings.TrimSuffix(path, `"`)
-	}
-
 	return path
 }
 
 func getFileSize(rdb *redis.Client, ctx context.Context, fullPath string) (int64, error) {
 	size, err := getFileSizeFromRedis(rdb, ctx, fullPath)
 	if err != nil {
-		fmt.Printf("Error getting size for %s: %v\n", fullPath, err)
+		log.Printf("Error getting size for %s: %v\n", fullPath, err)
 		return 0, err
 	}
 	return size, nil
