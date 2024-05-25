2f303b4e82c3d4ce8b2540d6861d155dcdca1add
Ying Huang <huangyingw@gmail.com>
2024-05-25 11:24:52 -0700

    n

--- main.go.sh
+++ main.go.sh
@@ -10,18 +10,14 @@ go get -u github.com/go-redis/redis/v8
 go get -u github.com/mattn/go-zglob/fastwalk
 go get -u github.com/karrick/godirwalk
 
-cp -v /media/secure_bcache/test/test.mp4.bak.bak /media/secure_bcache/test/test.mp4.bak
-cp -v /media/secure_bcache/test/test.mp4.bak.bak /media/secure_bcache/test/test.mp4
-docker-compose down -v
-docker-compose restart
+#cp -v /media/secure_bcache/test/test.mp4.bak.bak /media/secure_bcache/test/test.mp4.bak
+#cp -v /media/secure_bcache/test/test.mp4.bak.bak /media/secure_bcache/test/test.mp4
+#docker-compose down -v
+#docker-compose restart
 docker-compose up -d
 
-#rm /media/av162/cartoon/dragonball/test/*.txt
-#go run . /media/av162/cartoon/dragonball/test/
-#ls -al /media/av162/cartoon/dragonball/test/*.txt
-
 # 定义路径变量，确保处理包含空格和特殊字符的情况
-rootDir="/media/secure_bcache/test/"
+rootDir="/media/"
 
 # 正常运行
 go run . "$rootDir"
@@ -30,4 +26,4 @@ go run . "$rootDir"
 go run . "$rootDir" --find-duplicates
 
 # 删除重复文件（示例，实际运行时取消注释）
-go run . "$rootDir" --delete-duplicates
+# go run . "$rootDir" --delete-duplicates
