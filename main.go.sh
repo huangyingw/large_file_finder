2578b170e2151c6ec28a95d9575ae0d07f50ddd5
Ying Huang <huangyingw@gmail.com>
2024-05-25 20:38:09 -0700

    n

--- main.go.sh
+++ main.go.sh
@@ -10,17 +10,13 @@ go get -u github.com/go-redis/redis/v8
 go get -u github.com/mattn/go-zglob/fastwalk
 go get -u github.com/karrick/godirwalk
 
-#cp -v /media/secure_bcache/test/test.mp4.bak.bak /media/secure_bcache/test/test.mp4.bak
-#cp -v /media/secure_bcache/test/test.mp4.bak.bak /media/secure_bcache/test/test.mp4
-#docker-compose down -v
-#docker-compose restart
 docker-compose up -d
 
 # 定义路径变量，确保处理包含空格和特殊字符的情况
 rootDir="/media/"
 
 # 正常运行
-# go run . "$rootDir"
+go run . "$rootDir"
 
 # 输出重复文件结果
 go run . "$rootDir" --find-duplicates --max-duplicates=50
