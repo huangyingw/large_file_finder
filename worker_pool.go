f11806e844a725940167bfdf3ab5a76fb3562a48
Ying Huang <huangyingw@gmail.com>
2024-05-29 13:36:33 -0700

    n

--- worker_pool.go
+++ worker_pool.go
@@ -8,33 +8,23 @@ import (
 // Task 定义了工作池中的任务类型
 type Task func()
 
-func NewWorkerPool(workerCount int) (chan<- Task, *sync.WaitGroup, func(), chan struct{}) {
+func NewWorkerPool(workerCount int) (chan<- Task, *sync.WaitGroup, func()) {
 	var wg sync.WaitGroup
 	taskQueue := make(chan Task)
-	stopProcessing := make(chan struct{})
 
 	for i := 0; i < workerCount; i++ {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
-			for {
-				select {
-				case <-stopProcessing:
-					return
-				case task, ok := <-taskQueue:
-					if !ok {
-						return
-					}
-					task()
-				}
+			for task := range taskQueue {
+				task()
 			}
 		}()
 	}
 
 	stopFunc := func() {
-		close(stopProcessing) // 发送停止信号
-		close(taskQueue)      // 关闭任务队列
+		close(taskQueue) // 关闭任务队列
 	}
 
-	return taskQueue, &wg, stopFunc, stopProcessing
+	return taskQueue, &wg, stopFunc
 }
