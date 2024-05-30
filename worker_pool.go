56ac288568aa073484bbf682e98ce18f05691cbd
Ying Huang <huangyingw@gmail.com>
2024-05-30 12:08:47 -0700

    n

--- worker_pool.go
+++ worker_pool.go
@@ -3,28 +3,39 @@ package main
 
 import (
 	"sync"
+	"sync/atomic"
 )
 
 // Task 定义了工作池中的任务类型
 type Task func()
 
-func NewWorkerPool(workerCount int) (chan<- Task, *sync.WaitGroup, func()) {
+func NewWorkerPool(workerCount int, stopProcessing *int32) (chan<- Task, *sync.WaitGroup, func(), chan struct{}) {
 	var wg sync.WaitGroup
 	taskQueue := make(chan Task)
+	stopSignal := make(chan struct{})
 
 	for i := 0; i < workerCount; i++ {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
-			for task := range taskQueue {
-				task()
+			for {
+				select {
+				case <-stopSignal:
+					return
+				case task, ok := <-taskQueue:
+					if !ok || atomic.LoadInt32(stopProcessing) != 0 {
+						return
+					}
+					task()
+				}
 			}
 		}()
 	}
 
 	stopFunc := func() {
-		close(taskQueue) // 关闭任务队列
+		close(stopSignal) // 发送停止信号
+		close(taskQueue)  // 关闭任务队列
 	}
 
-	return taskQueue, &wg, stopFunc
+	return taskQueue, &wg, stopFunc, stopSignal
 }
