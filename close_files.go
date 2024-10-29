13c2228e529a899907b7d2cd84408955b780eb43
Ying Huang <huangyingw@gmail.com>
2024-10-28 23:41:34 -0700

    n

--- close_files.go
+++ close_files.go
@@ -164,51 +164,6 @@ func (cf *CloseFileFinder) writeResults(results []similarityResult) error {
 	return writer.Flush()
 }
 
-// Levenshtein 距离计算
-func levenshteinDistance(s1, s2 string) int {
-	if len(s1) == 0 {
-		return len(s2)
-	}
-	if len(s2) == 0 {
-		return len(s1)
-	}
-
-	matrix := make([][]int, len(s1)+1)
-	for i := range matrix {
-		matrix[i] = make([]int, len(s2)+1)
-		matrix[i][0] = i
-	}
-	for j := range matrix[0] {
-		matrix[0][j] = j
-	}
-
-	for i := 1; i <= len(s1); i++ {
-		for j := 1; j <= len(s2); j++ {
-			cost := 1
-			if s1[i-1] == s2[j-1] {
-				cost = 0
-			}
-			matrix[i][j] = min(
-				matrix[i-1][j]+1,
-				matrix[i][j-1]+1,
-				matrix[i-1][j-1]+cost,
-			)
-		}
-	}
-
-	return matrix[len(s1)][len(s2)]
-}
-
-func min(nums ...int) int {
-	result := nums[0]
-	for _, num := range nums[1:] {
-		if num < result {
-			result = num
-		}
-	}
-	return result
-}
-
 func max(a, b int) int {
 	if a > b {
 		return a
